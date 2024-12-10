e701cba8d51160b4624fd3989c19f1329b5bb164
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-12-10 03:52:55 +0100

    io.py: cleaner CLI

--- src/h2scenarios/io.py
+++ src/h2scenarios/io.py
@@ -1,3 +1,5 @@
+"""Data cleanup utilities."""
+
 from collections import defaultdict
 from pathlib import Path
 
@@ -7,6 +9,7 @@ import xarray as xr
 
 
 def csv_to_dfs(dirname: str):
+    """Read CSVs from dir tree, and convert to `pandas.DataFrame`."""
     template = "{which_data}_{technology:l}_{distance:d}_{bound_eco:l}_{bound_tech:l}_{pipe_kind:l}_pipe.csv"
     dst = Path(dirname)
     csvs = list(dst.glob("**/*.csv"))
@@ -15,21 +18,23 @@ def csv_to_dfs(dirname: str):
     for c in csvs:
         parse_result = parse.parse(template, c.name)
         which_data = parse_result.named["which_data"]
-        del parse_result.named["which_data"]  # this key is not needed in the dataframes themselves
+        # this key is not needed in the dataframes themselves
+        del parse_result.named["which_data"]
 
         df = pd.read_csv(c, index_col=0).assign(**parse_result.named)
 
-        if which_data in ["sistem costs", "system costs"]:  # prepared for having the 'sistem' typo be fixed
+        # prepared for having the 'sistem' typo be fixed
+        if which_data in ["sistem costs", "system costs"]:
             # Use component name as part of index
             columns_to_index = df.columns[-5:].values.tolist() + [df.columns[0]]
 
-        elif which_data == 'simp_output_results':
+        elif which_data == "simp_output_results":
             # ignore index, only has a single entry
             columns_to_index = df.columns[-5:].values.tolist()
 
-        elif which_data == 'time_dep_costs':
+        elif which_data == "time_dep_costs":
             # use index as year count
-            df['year'] = df.index.values
+            df["year"] = df.index.values
             columns_to_index = df.columns[-6:].values.tolist()
 
         df = df.set_index(columns_to_index)
@@ -56,16 +61,23 @@ def multi_indexed_dfs_to_xarray(multi_indexed_dfs):
     ds = xr.merge(xr.Dataset.from_dataframe(df) for df in multi_indexed_dfs.values())
 
     # '/' is not accepted as a variable name by NetCDF, rename to '_per_'
-    renaming = {old: old.replace('/', '_per_') for old in ds.data_vars.keys()}
+    renaming = {old: old.replace("/", "_per_") for old in ds.data_vars.keys()}
     ds = ds.rename_vars(renaming)
     return ds
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
 
-    multi_indexed_dfs = csv_to_dfs('data/')
+    parser = ArgumentParser(__doc__)
+    parser.add_argument("input_dir")
+    opts = parser.parse_args()
+
+    multi_indexed_dfs = csv_to_dfs(opts.input_dir)
 
     ds = multi_indexed_dfs_to_xarray(multi_indexed_dfs)
 
     # Export!
-    ds.to_netcdf('data-Laurens-HyChain_WP1.nc')
+    outdir = opts.input_dir.rsplit("/", maxsplit=1)[0]
+    print(f"Exporting to {outdir}")
+    ds.to_netcdf(f"{outdir}/data-Laurens-HyChain_WP1.nc")
